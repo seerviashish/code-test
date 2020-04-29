fa97e60d59a2ffb847f9f1565ac5581c4da91859
Ashish Seervi <seerviashish17@gmail.com>
2020-04-29 13:11:32 +0530

    fast scanner

--- JavaTest.java
+++ JavaTest.java
@@ -1,16 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.InputMismatchException;
+import java.util.StringTokenizer;
 
 public class JavaTest {
     private static PrintWriter out = new PrintWriter(System.out);
-    private static InputStreamReader in;
+    private static FastScanner sc;
 
     public static void main(String args[]) throws Exception {
         InputStream is = JavaTest.class.getResourceAsStream("in.txt");
         boolean testMode = is != null;
-        in = new InputStreamReader(testMode ? is : System.in);
+        sc = new FastScanner(testMode ? is : System.in);
 
         long start = System.currentTimeMillis();
         main();
@@ -24,88 +26,39 @@ public class JavaTest {
     }
 
     private static void main() throws Exception {
-        int n = in.readInt();
-        int k = in.readInt();
-        int[] arr = new int[n];
-        int cnt = 0;
-        for (int i = 0; i < n; i++)
-            arr[i] = in.readInt();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (Math.abs(arr[i] - arr[j]) == k) {
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-
+        int t = sc.nextInt();
+        out.println(t);
     }
 
-    private static class InputStreamReader {
-        private byte[] buf = new byte[1 << 8];
-        private int curChar;
-        private int charsRead;
-        private InputStream stream;
+    private static class FastScanner {
+        private BufferedReader br;
+        private StringTokenizer st;
 
-        public InputStreamReader(InputStream stream) {
-            this.stream = stream;
+        public FastScanner(InputStream stream) {
+            br = new BufferedReader(new InputStreamReader(stream));
         }
 
-        public int readInt() throws IOException {
-            int c = read();
-            while (skip(c))
-                c = read();
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
+        public String nextToken() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            int res = 0;
-            do {
-                if (c < '0' || c > '9')
-                    throw new InputMismatchException();
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!skip(c));
-            return res * sgn;
+            return st.nextToken();
         }
 
-        public long readLong() throws IOException {
-            int c = read();
-            while (skip(c))
-                c = read();
-            int sign = 1;
-            if (c == '-') {
-                sign = -1;
-                c = read();
-            }
-            long res = 0;
-            do {
-                if (c < '0' || c > '9')
-                    throw new InputMismatchException();
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!skip(c));
-            return res * sign;
+        public int nextInt() {
+            return Integer.parseInt(nextToken());
         }
 
-        private int read() throws IOException {
-            if (charsRead == -1)
-                throw new InputMismatchException();
-            if (curChar >= charsRead) {
-                curChar = 0;
-                charsRead = stream.read(buf);
-                if (charsRead <= 0)
-                    return -1;
-            }
-            return buf[curChar++];
+        public long nextLong() {
+            return Long.parseLong(nextToken());
         }
 
-        private static boolean skip(int c) {
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        public double nextDouble() {
+            return Double.parseDouble(nextToken());
         }
     }
 }
