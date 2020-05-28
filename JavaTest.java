4e3834a6892197cd15abf71ae260e4af1ba8ddf4
Ashish Seervi <seerviashish17@gmail.com>
2020-05-29 02:22:21 +0530

    code: 4-D

--- JavaTest.java
+++ JavaTest.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class JavaTest {
@@ -25,11 +27,78 @@ public class JavaTest {
         out.close();
     }
 
+    static class Envelope {
+        int w;
+        int h;
+        int i;
+
+        public Envelope(int w, int h, int i) {
+            this.w = w;
+            this.h = h;
+            this.i = i;
+        }
+
+    }
+
     private static void main() throws Exception {
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int num = sc.nextInt();
-            System.out.println(num);
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        ArrayList<Envelope> arr = new ArrayList<>();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int wi = sc.nextInt();
+            int hi = sc.nextInt();
+            if (wi > w && hi > h) {
+                arr.add(new Envelope(wi, hi, i));
+                cnt++;
+            }
+        }
+        if (cnt == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        arr.sort((e1, e2) -> {
+            int res = e1.h - e2.h;
+            if (res == 0) {
+                res = e1.w - e2.w;
+            }
+            return res;
+        });
+
+        int[] dp = new int[cnt];
+        int[] next = new int[cnt];
+        Arrays.fill(dp, 1);
+        Arrays.fill(next, -1);
+
+        for (int i = cnt - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                Envelope ai = arr.get(i);
+                Envelope aj = arr.get(j);
+                if (ai.w > aj.w && ai.h > aj.h) {
+                    if (dp[j] < dp[i] + 1) {
+                        dp[j] = dp[i] + 1;
+                        next[j] = i;
+                    }
+                }
+            }
+        }
+
+        int res = 1;
+        int head = 0;
+
+        for (int i = 0; i < cnt; i++) {
+            if (dp[i] > res) {
+                res = dp[i];
+                head = i;
+            }
+        }
+
+        System.out.println(res);
+        while (head != -1) {
+            System.out.println(arr.get(head).i + 1 + " ");
+            head = next[head];
         }
     }
 
