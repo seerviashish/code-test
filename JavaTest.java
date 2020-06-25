fa94a953895ed0f89f3832b8defd91616475e639
Ashish Seervi <seerviashish17@gmail.com>
2020-06-26 01:28:29 +0530

    fix

--- JavaTest.java
+++ JavaTest.java
@@ -1,18 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class JavaTest {
     private static PrintWriter out = new PrintWriter(System.out);
-    private static FastScanner sc;
+    private static Scanner sc;
 
     public static void main(String args[]) throws Exception {
         InputStream is = JavaTest.class.getResourceAsStream("in.txt");
         boolean testMode = is != null;
-        sc = new FastScanner(testMode ? is : System.in);
+        sc = new Scanner(testMode ? is : System.in);
 
         long start = System.currentTimeMillis();
         main();
@@ -27,41 +24,7 @@ public class JavaTest {
 
     private static void main() throws Exception {
         int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int num = sc.nextInt();
-            System.out.println(num);
-        }
+        System.out.println(t);
     }
 
-    private static class FastScanner {
-        private BufferedReader br;
-        private StringTokenizer st;
-
-        public FastScanner(InputStream stream) {
-            br = new BufferedReader(new InputStreamReader(stream));
-        }
-
-        public String nextToken() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(nextToken());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(nextToken());
-        }
-
-        public double nextDouble() {
-            return Double.parseDouble(nextToken());
-        }
-    }
 }
